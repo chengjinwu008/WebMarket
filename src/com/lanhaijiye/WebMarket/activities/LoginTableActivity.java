b2dba75b549c264893af32e708606e8cbdf0efc8
Chen <chengjinwu008@qq.com>
2015-05-07 09:27:40 +0800

    ok

--- src/com/lanhaijiye/WebMarket/activities/LoginTableActivity.java
+++ src/com/lanhaijiye/WebMarket/activities/LoginTableActivity.java
@@ -1,5 +1,6 @@
 package com.lanhaijiye.WebMarket.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -10,7 +11,10 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 import com.lanhaijiye.WebMarket.R;
+import com.lanhaijiye.WebMarket.adapter.TextAdapter;
 import com.lanhaijiye.WebMarket.dao.AccountData;
+import com.lanhaijiye.WebMarket.utils.DimensionUtil;
+import com.lanhaijiye.WebMarket.utils.PopWindowUtil;
 
 /**
  * Created by Administrator on 2015/5/5.
@@ -21,6 +25,8 @@ public class LoginTableActivity extends BaseActivity implements CompoundButton.O
     private EditText user_name_text;
     private CheckBox checkBox;
     private EditText login_password_text;
+    private ListView accountsList;
+    private PopupWindow pop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +77,20 @@ public class LoginTableActivity extends BaseActivity implements CompoundButton.O
             case R.id.login_username_delete:
                 String[] accounts= AccountData.getAccounts(this);
 
+                //todo 弹出账号记录
+//                String[] accounts = new String[]{"1","1","1","1","1"};
+                TextAdapter adapter = new TextAdapter(accounts,this);
+                if(accountsList==null) {
+                    accountsList = new ListView(this);
+                    accountsList.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+
+                    accountsList.setBackgroundResource(R.drawable.white_with_black_border);
+                    accountsList.setDivider(null);
+                }
+                accountsList.setAdapter(adapter);
+                if(pop==null)
+                pop = new PopupWindow(user_name_text.getMeasuredWidth(), (int) DimensionUtil.getPixel(150,this.getResources().getDisplayMetrics()));
+                PopWindowUtil.show(accountsList,user_name_text,null,pop,R.style.account_list_anim);
                 break;
         }
     }
--- src/com/lanhaijiye/WebMarket/utils/popWindowUtil.java
+++ src/com/lanhaijiye/WebMarket/utils/popWindowUtil.java
@@ -3,21 +3,25 @@ package com.lanhaijiye.WebMarket.utils;
 import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 
 /**
  * Created by Administrator on 2015/5/6.
  */
-public class popWindowUtil {
+public class PopWindowUtil {
 
-    public static void show(View viewToShow,View viewToAttach,PopupWindow window,int AnimationStyle){
+    public static void show(View viewToShow,View viewToAttach,View parentView,PopupWindow window,int AnimationStyle){
         window.setContentView(viewToShow);
         window.setAnimationStyle(AnimationStyle);
         window.setTouchable(true);
         window.setOutsideTouchable(false);
         window.setFocusable(true);
         window.setBackgroundDrawable(new ColorDrawable(0));
-        window.showAtLocation(viewToAttach, Gravity.CENTER, 0, 0);
+        if(viewToAttach==null && parentView!=null)
+        window.showAtLocation(parentView, Gravity.CENTER, 0, 0);
+        else if(viewToAttach!=null)
+            window.showAsDropDown(viewToAttach);
     }
-
 }
