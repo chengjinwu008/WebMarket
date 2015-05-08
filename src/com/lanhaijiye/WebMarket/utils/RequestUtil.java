9f8e94b7548282d9a81cffc074f40a48486c84ba
Chen <chengjinwu008@qq.com>
2015-05-09 03:25:28 +0800

    xxx

---
+++ src/com/lanhaijiye/WebMarket/utils/RequestUtil.java
@@ -0,0 +1,64 @@
+package com.lanhaijiye.WebMarket.utils;
+
+import javax.xml.validation.Validator;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * Created by CJQ on 2015/5/9.
+ */
+public class RequestUtil {
+
+    public enum RequestMethod{
+        POST,GET
+    }
+
+    //发送请求返回字节数组
+    public static byte[] requestURLWithParameter(RequestMethod method,String url,Map<String,String> parameters,int succeedCode,StreamUtil.StreamListener listener) throws IOException {
+        byte[] res =null;
+        StringBuilder builder = new StringBuilder();
+        for(Map.Entry<String,String> item:parameters.entrySet()){
+            builder.append(item.getKey()).append("=").append(item.getValue()).append("&");
+        }
+        HttpURLConnection connection=null;
+        switch (method){
+            case POST:
+                //发送的是post请求
+                URL urlAddress = new URL(url);
+                connection = (HttpURLConnection) urlAddress.openConnection();
+                connection.setRequestMethod(method.toString());
+                connection.setConnectTimeout(NetWorkUtils.TIME_OUT);
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.connect();
+                BufferedOutputStream out = new BufferedOutputStream(connection.getOutputStream());
+                out.write(builder.toString().getBytes());
+                out.flush();
+                out.close();
+                break;
+            case GET:
+                url+="?"+builder.toString();
+                connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod(method.toString());
+                connection.setConnectTimeout(NetWorkUtils.TIME_OUT);
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.connect();
+                break;
+        }
+        if(connection.getResponseCode()==succeedCode){
+            InputStream in =new BufferedInputStream(connection.getInputStream());
+            res=StreamUtil.readStreamToBytes(in,connection.getContentLength(),listener);
+            in.close();
+        }
+        connection.disconnect();
+        return res;
+    }
+
+}
--- src/com/lanhaijiye/WebMarket/utils/StreamUtil.java
+++ src/com/lanhaijiye/WebMarket/utils/StreamUtil.java
@@ -71,4 +71,31 @@ public class StreamUtil {
         out.flush();
         bytes = null;
     }
+
+    /**
+     * 读取流并以字节数组的形式返回
+     *
+     * @param stream    需要读取的流
+     * @param maxLength 流的长度（仅用于计算进度）
+     * @param listener  进度监听
+     * @return 字节数组
+     * @throws IOException
+     */
+    public static byte[] readStreamToBytes(InputStream stream, int maxLength, StreamListener listener) throws IOException {
+        byte[] bytes = new byte[buffer_size];
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int len;
+        int progress = 0;
+        while ((len = stream.read(bytes)) != -1) {
+            out.write(bytes, 0, len);
+            progress += len;
+            if (listener != null)
+                listener.onProgressUpdate(progress / maxLength * 100);
+        }
+        out.flush();
+        String res = out.toString();
+        out.close();
+        bytes = null;
+        return res.getBytes();
+    }
 }
